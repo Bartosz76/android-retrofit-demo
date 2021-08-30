ffcbb7ffefae524274d1966509535c6ef3c07e9c
Bartosz76 <mierzwinski.b@gmail.com>
2021-08-30 08:28:59 +0200

    The initial commit with retrofit and GSON dependencies.

---
+++ app/src/main/java/bm/app/android_retrofit_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package bm.app.android_retrofit_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
